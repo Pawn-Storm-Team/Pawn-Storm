e89345b846d5bcc7932a908dfa3a2345722c4499
jbedette <john.bedette45@gmail.com>
2021-08-10 13:43:13 -0700

    merging with dev

--- check_move.h
+++ check_move.h
@@ -1,8 +1,6 @@
-/*
 #include "board.h"
 
 int agnostic_check(chessboard * game, int init_rank,int init_file,int dest_rank,int dest_file);
-
 int white_pawn_check(chessboard * game, int init_rank,int init_file,int dest_rank,int dest_file);
 int black_pawn_check(chessboard * game, int init_rank,int init_file,int dest_rank,int dest_file);
 int knight_check(chessboard * game, int init_rank,int init_file,int dest_rank,int dest_file);
@@ -10,4 +8,3 @@ int bishop_check(chessboard * game, int init_rank,int init_file,int dest_rank,in
 int rook_check(chessboard * game, int init_rank,int init_file,int dest_rank,int dest_file);
 int queen_check(chessboard * game, int init_rank,int init_file,int dest_rank,int dest_file);
 int king_check(chessboard * game, int init_rank,int init_file,int dest_rank,int dest_file);
-*/
--- interface.cpp
+++ interface.cpp
@@ -31,6 +31,7 @@ int Interface::menu(){
     //intro();
     board.initialize();
     action_prompt();
+    return 0;
 }
 int Interface::intro() {
 
@@ -97,6 +98,7 @@ int Interface::action_prompt(){
 }
 
 
+//turn prompting
 int Interface::turn_prompt(){
 
     //check member for who's turn it is, set string to white or black
@@ -130,15 +132,18 @@ int Interface::turn_prompt(){
         cin.ignore();
     }
 
-    //a is origin, b is dest
+    //ab is origin, xy is dest
+    //by this point we know they're numbers between 1 and 8
+    //subtractin char 1 and storing them in int will extract the numbers
     int a, b, x, y, out;
-    a = input[0] - '0';
-    b = input[2] - '0';
-    x = input[4] - '0';
-    y = input[6] - '0';
+    a = input[0] - '1';
+    b = input[2] - '1';
+    x = input[4] - '1';
+    y = input[6] - '1';
     return board.make_move(a,b,x,y);
 }
 
+//this here verifies that given char represents a number within our desired range
 int range_validator(char a){
     if(isdigit(a)){
         int b = a - '0';
@@ -148,6 +153,7 @@ int range_validator(char a){
     return 1;
 }
 
+//this checks all four input values
 //we're subtracting double negative from valid, so if any of these pops a "YES this DOESNT work" we get back invalid 
 bool Interface::move_format_validator(char a, char b, char x, char y){
     int valid = 0;
